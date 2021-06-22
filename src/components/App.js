e1c1625bd556409461cb712490130aefb1905b33
unknown <yasodatalapu227@gmail.com>
2021-06-22 10:35:54 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,123 @@
-import React, { Component, useState,useEffect } from "react";
+import React, {Component, useState,useEffect} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  const [name, setname] = useState('');
-  const [email, setemail] = useState('');
-  const [gender, setgender] = useState('male');
-  const [phone, setphone] = useState('');
-  const [password, setpassword] = useState('');
-  const [success, setsuccess] = useState(false);
-  const [message, setmessage] = useState('');
-  const [error, seterror] = useState(null)
-  useEffect(() => {
-    setemail('');
-    setgender('');
-    setphone('');
-    setpassword('');
-    setname('');
-  }, [success])
-  const submitForm = (e) => {
-    e.preventDefault();
-    if (name === '' && email === '' && gender === '' && phone === '' && password === '') {
-      alert("All fields are mandatory");
-    }
-    else {
-      if (name === '') { seterror("Name Error") }
-      else if (email === '') { seterror("Email Error") }
-      else if (phone === '') { seterror("Phone Number Error") }
-      else if (password === '') { seterror("Password Error") }
-      else {
-        let alphaExp = /^([a-z]+[\s]*[0-9]+[\s]*)+$/i;
-        if (!alphaExp.test(name)) {
-          seterror("Name is not alphanumeric")
-        }
-        else if (!email.includes('@')) {
-          seterror("Email must contain @")
-        }
-        else if (gender === "") {
-          seterror("Please identify as male, female or others")
-        }
-        else if (isNaN(phone)) {
-          seterror("Phone Number must contain only numbers")
-        }
-        else if (password.length < 6) {
-          seterror("Password must contain atleast 6 letters")
-        } else {
-          setsuccess(true);
-          seterror(null);
-          let username = email.split("@");
-          setmessage(`Hello ${username[0]}`);
-        }
-      }
-    }
-  }
+  
+   const [getName,setName] = useState("");
+   const [getEmail,setEmail] = useState("");
+   const [getGender,setGender] = useState("male");
+   const [getPhone,setPhone] = useState("");
+   const [getPassword,setPassword] = useState("");
+   const [getError,setError] = useState("");
+   const [getSuccess,setSuccess] = useState(false);
+   const [getMsg,setMsg] = useState("");
+   
+   useEffect(()=>{
+      setPassword("");
+      setPhone("");
+      setName("");
+      setEmail("");
+      setGender("");
+   },[getSuccess]);
+   
+   const putNameHandler=(e)=>{
+       setName(e.target.value);
+   }
+   const putEmailHandler=(e)=>{
+       setEmail(e.target.value);
+   }
+   const putGenderHandler=(e)=>{
+      setGender(e.target.value);
+   }
+   const putPhoneHandler=(e)=>{
+      setPhone(e.target.value);
+   }
+   const putPasswordHandler=(e)=>{
+       setPassword(e.target.value);
+   }
+   
+   const submitHandler = (e)=>{
+       e.preventDefault();
+       if(getName==="" && getEmail==="" && getPassword==="" && getGender==="" && getPhone===""){
+          setError("All fields are mandatory");
+       }
+       else{
+          if(getName===""){
+             setError("Name Error");
+          }
+          else if(getEmail===""){
+             setError("Email Error");
+          } 
+          else if(getPhone===""){
+            setError("Phone Number Error");
+          }
+          else if(getPassword===""){
+             setError("Password Error");
+          }else{
+               
+            let alphaExp=/^([a-z]+[\s]*[0-9]+[\s]*)+$/i;
+            if(!alphaExp.test(getName)){
+               setError("Name is not alphanumeric");
+            }else{
+                 
+                if(!getEmail.includes("@")){
+                     setError("Email must contain @")
+                }else{
+                    if(getGender===""){
+                       setError("Please identify as male, female or others")
+                    }else{
+                       if(isNaN(getPhone)){
+                        setError("Phone Number must contain only numbers")
+                       }else{
+                          
+                        if(getPassword.length<6){
+                           setError("Password must contain atleast 6 letters")
+                        }
+                        else{
+                           setSuccess(true);
+                           let out = getEmail.split("@");
+                           setMsg(out[0]);
+                          
+                           
+
+                        }
+                     
+                       }
+                    }
+                }
+                
+            }
+              
+
+          }         
+       } 
+          
+       
+       
+   }
+
   return (
     <div id="main">
-        <form>
-      {!success && error}
-      {success && message}
-        <div className="mb-3">
-        <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" data-testid="name" onChange={(e)=>setname(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" data-testid="email" onChange={(e)=>setemail(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-        <label htmlFor="email">Gender</label>
-          <select className="form-select" data-testid = 'gender' onChange={(e)=>setgender(e.target.value)}>
-            <option value="male">male</option>
-            <option value="female" >female</option>
-            <option value="other">other</option>
-         </select>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="phone">Phone Number</label>
-          <input type="number" className="form-control" data-testid = 'phoneNumber' onChange={(e)=>setphone(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-        <label htmlFor="phone">Password</label>
-          <input type="password" className="form-control" data-testid = 'password' onChange={(e)=>setpassword(e.target.value)}/>
-        </div>
-        <button type="submit" className="btn btn-primary" data-testid = 'submit' onClick={submitForm}>Submit</button>
-    </form>
+       <form>
+           {!getSuccess && getError}
+           {getSuccess && getMsg}
+          <br></br>
+          Name<input type="text" value={getName} data-testid = 'name' onChange={putNameHandler}/>
+          <br></br>
+          Email address<input value={getEmail} type="text" data-testid = 'email' onChange={putEmailHandler} />
+          <br></br>
+          Gender<select  data-testid='gender' value={getGender}  onChange={putGenderHandler}>
+             <option value="male">male</option>
+             <option value="female">female</option>
+             <option value="other">other</option>
+             </select>
+          <br></br>
+          Phone Number <input type="text" value={getPhone} data-testid = 'phoneNumber' onChange={putPhoneHandler}/>
+          <br></br>
+          Password <input data-testid = 'password' value={getPassword} type="password" onChange={putPasswordHandler}/>
+          <br></br>
+          Submit button <button type="submit" data-testid = 'submit'  onClick={submitHandler} >Submit</button>
+       </form>
     </div>
   )
 }
